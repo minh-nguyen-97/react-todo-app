ac0977a52d4cdf46cd0d437510cd84b959f34cdd
AchillesS <qmn1312@gmail.com>
2019-09-24 01:02:37 +1000

    Setup firestore config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import * as firebase from 'firebase/app'
+
+const firebaseConfig = require('./config/firebase-config')
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
