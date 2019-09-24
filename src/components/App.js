28a5229a6cc2b9def405c296275a59e38e70b4f5
AchillesS <qmn1312@gmail.com>
2019-09-24 14:56:49 +1000

    Finish connecting database to firestore

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,22 @@ import { Container } from '@material-ui/core';
 import Header from './Header';
 import { db } from '../config/firebase'
 
+const todosRef = db.collection('todos')
+
 class App extends React.Component {
 
   state = {
     items: []
+  }
+
+  componentDidMount() {
+    (async () => {
+      const snapshot = await db.collection('todos').get()
+      const todos = snapshot.docs.map(doc => doc.data().taskName);
+      this.setState({
+        items: todos
+      })
+    })()
   }
 
   addItem = (newItem) => {
@@ -17,11 +29,19 @@ class App extends React.Component {
     } else if (this.state.items.includes(newItem)) {
       return 'Task is already existed'
     }
+    
+    todosRef.add({
+      taskName: newItem
+    })
 
     this.setState((prevState) => ({items: [...prevState.items, newItem]}))
   }
 
   removeItem = (removedItem) => {
+    todosRef.where('taskName', '==', removedItem).get().then(snapshot => {
+      snapshot.docs.forEach(doc => doc.ref.delete())
+    })
+
     this.setState((prevState) => ({
       items: prevState.items.filter((item) => (item !== removedItem))
     }))
