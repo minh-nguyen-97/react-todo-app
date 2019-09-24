2ac11b64abea1c939b1f0a4c6e00c713fe57a6e4
AchillesS <qmn1312@gmail.com>
2019-09-24 15:53:26 +1000

    firebase Config ...

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
-}
+  apiKey: "AIzaSyDZSxo1v0CYxwmHHs769dqjiUvfxofYFV8",
+  authDomain: "react-todo-app-13121997.firebaseapp.com",
+  databaseURL: "https://react-todo-app-13121997.firebaseio.com",
+  projectId: "react-todo-app-13121997",
+  storageBucket: "react-todo-app-13121997.appspot.com",
+  messagingSenderId: "201430146812",
+  appId: "1:201430146812:web:b58986d93e84a340110179"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 
